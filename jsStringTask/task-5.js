6fa20e7580f3c8da6f4ca9d2af29f35880a8e061
RashikChowdhury <muhammadrashikchowdhury@gmail.com>
2025-02-15 13:36:07 +0600

    updated

--- jsStringTask/task-5.js
+++ jsStringTask/task-5.js
@@ -6,3 +6,10 @@ const str = 'my name is muhammad rashik chowdhury';
 // console.log(str.charAt(0).toUpperCase() + str.slice(1)) //just making the first letter of this sentence to uppercase
 
 console.log(str.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')) //here .map is a type of loop(shorthand) and => is shorthand arrow function.
+
+/* 
+.map() loops through an array.
+It modifies each element based on the function.
+It returns a new array (original array remains unchanged).
+Arrow functions (=>) make it shorter.
+*/
