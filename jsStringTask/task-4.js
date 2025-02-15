45122408b9e4b2f5aadd4e51bbc0ec3f23d7f0f0
RashikChowdhury <muhammadrashikchowdhury@gmail.com>
2025-02-15 13:34:48 +0600

    updated

--- jsStringTask/task-4.js
+++ jsStringTask/task-4.js
@@ -1,3 +1,12 @@
 // question: If a given string has either x, replace x by y. if the given string has X, replace it by Y.
 
 // Hint: You should be able to check whether x or X exists. After that, search online how to replace a character in a string.
+
+const str = "eXample text with x and X";
+
+
+// console.log(str.split('x').join('y').split('X').join('Y')) //replacing strings using split and join 
+
+console.log(str.replaceAll('x', 'y').replaceAll('X', 'Y')); //replacing strings using replaceAll
+
+// console.log(str.replace(/x/g, 'y').replace(/X/g, 'Y'));
--- jsStringTask/task-5.js
+++ jsStringTask/task-5.js
@@ -1,1 +1,8 @@
 // question: Capitalize Every first Letter of each word in a String
+
+const str = 'my name is muhammad rashik chowdhury';
+
+
+// console.log(str.charAt(0).toUpperCase() + str.slice(1)) //just making the first letter of this sentence to uppercase
+
+console.log(str.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')) //here .map is a type of loop(shorthand) and => is shorthand arrow function.
