9675a9c0fc35b379ad235defbba25a9faa5556b0
RashikChowdhury <muhammadrashikchowdhury@gmail.com>
2025-02-19 15:35:01 +0600

    created a file called array.js

---
+++ array.js
@@ -0,0 +1,32 @@
+// looping an array with 3 diff types of loop
+
+
+// ****** for of
+
+// const names = ['rashik', 'rayhan', 'tahi', 'sayem', 'tasin', 'tuhin'];
+
+// for (let name of names){
+//     console.log(name);
+// }
+
+
+// ****** for
+
+// const names = ['rashik', 'rayhan', 'tahi', 'sayem', 'tasin', 'tuhin'];
+
+// for (let i = 0; i < names.length; i++){
+//     console.log(names[i]);
+// }
+
+
+
+// ******* while
+
+const names = ['rashik', 'rayhan', 'tahi', 'sayem', 'tasin', 'tuhin'];
+
+let i = 0;
+
+while (i < names.length){
+    console.log(names[i]);
+    i++;
+}
