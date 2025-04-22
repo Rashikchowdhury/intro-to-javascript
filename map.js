3fbd7f481bb7b19d21e9cf9bc90d28d920882a54
RashikChowdhury <muhammadrashikchowdhury@gmail.com>
2025-04-22 06:35:47 +0600

    how to use map() in js

---
+++ map.js
@@ -0,0 +1,29 @@
+// map => Loops through each elements of the array and do the operation that you passed in the call back function and hold the result from each operation in an array and finally returns you the array.
+
+
+// type one
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// function doubleIt(num){
+//     return num * 2;
+// }
+
+// const doubled = numbers.map(doubleIt);
+// console.log(doubled);
+
+
+// type two:
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const doubleIt = num => num * 2;
+
+// const doubled = numbers.map(doubleIt);
+// console.log(doubled);
+
+
+// type three
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+const doubled = numbers.map(num => num * 2);
+console.log(doubled);
