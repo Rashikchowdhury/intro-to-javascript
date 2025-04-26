04a3947dfe8648ee7bc5729dbe98f47fef7b89ed
RashikChowdhury <muhammadrashikchowdhury@gmail.com>
2025-04-26 16:41:31 +0600

    split terminal

--- map.js
+++ map.js
@@ -1,16 +1,28 @@
 // map => Loops through each elements of the array and do the operation that you passed in the call back function and hold the result from each operation in an array and finally returns you the array.
 
-
-// type one
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
+// ******** without map:
 // function doubleIt(num){
-//     return num * 2;
+//     let arr = [];
+//     for (let no of num){
+//         arr.push(no * 2);
+//     }
+//     return arr;
 // }
 
-// const doubled = numbers.map(doubleIt);
-// console.log(doubled);
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+// console.log(doubleIt(numbers));
+
+
+// type one
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function doubleIt(num){
+    return num * 2;
+}
+
+const doubled = numbers.map(doubleIt);
+console.log(doubled);
 
 // type two:
 // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -22,8 +34,8 @@
 
 
 // type three
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
-const doubled = numbers.map(num => num * 2);
-console.log(doubled);
+// const doubled = numbers.map(num => num * 2);
+// console.log(doubled);
