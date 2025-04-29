206a7bed9625c8faa61916a82e8e481a47caf363
RashikChowdhury <muhammadrashikchowdhury@gmail.com>
2025-04-29 10:14:16 +0600

    created a folder named common-js-concepts and created a file called types.js into the folder and learn about the datatypes in javascript

---
+++ common-js-concepts/types.js
@@ -0,0 +1,29 @@
+// javascript is a dynamic typed language
+
+/*
+Data types in javascript: there ar two types :
+
+1. Primitive types --- Numbers , Strings, Boolean, Null, Undefined.
+2. Non-primitive types --- Objects & Arrays.
+*/
+
+
+// primitive types:
+
+let number = 10;
+const name = "Rashik";
+let fail = true;
+let nul = 0;
+let roll;
+
+console.log(typeof number, typeof name, typeof fail, typeof nul, typeof roll);
+
+
+// Non-primitive types:
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let people = {
+    name: "rashik", 
+    profession : "Web developer",
+};
+
+console.log(Array.isArray(numbers) , typeof people);
