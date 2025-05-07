553b6582ffb5424e7333befeb14ea48fa3b34134
Rashikchowdhury <muhammadrashikchowdhury@gmail.com>
2025-05-07 06:12:21 +0600

    fetching data using await and async

--- API/js/app.js
+++ API/js/app.js
@@ -1,20 +1,32 @@
-function loadData2(){
+function loadData2() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => console.log(data))
+        .then(res => res.json())
+        .then(data => console.log(data))
 }
 
-function loadPhotos(){
+function loadPhotos() {
     fetch('https://jsonplaceholder.typicode.com/photos')
-    .then(res => res.json())
-    .then(data => displayPhotos(data))
+        .then(res => res.json())
+        .then(data => displayPhotos(data))
 }
 
-function displayPhotos(photos){
+function displayPhotos(photos) {
     const ul = document.getElementById('image-titles');
-    for(let photo of photos){
+    for (let photo of photos) {
         let li = document.createElement('li');
         li.innerText = photo.title;
         ul.appendChild(li);
     }
+}
+
+const loadComments = async () => {
+    try {
+        let res = await fetch('https://jsonplaceholder.typicode.com/comments');
+        let data = await res.json();
+        console.log(data);
+    }
+    catch(error){
+        console.error("You did somthing wrong:", error)
+    }
+    
 }
