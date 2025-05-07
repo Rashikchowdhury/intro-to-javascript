b8c9bc4a67d1716c4cf3b76d1fc14ae63b319502
Rashikchowdhury <muhammadrashikchowdhury@gmail.com>
2025-05-07 19:42:18 +0600

    created pattern with hash

---
+++ pattern.js
@@ -0,0 +1,13 @@
+// let hash = 1;
+
+// for (let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+
+let i = 0;
+let hash = '';
+while (i < 10) {
+    hash += '#';
+    console.log(hash);
+    i++;
+}
